2c268de8520f572ce2c6e3e86c255a4240a900a7
Ivan Baranov <293344asd@gmail.com>
2019-11-04 20:24:01 +0600

    EL-22: неболшие правки в стили

--- front/src/Components/Pagination/styles.ts
+++ front/src/Components/Pagination/styles.ts
@@ -3,7 +3,7 @@ import styled, { css } from 'styled-components';
 import { StyledBtnProps } from './types';
 
 export const StyledContainer = styled.div`
-  position:absolute;
+  position: fixed;
   bottom: 20px;
   left: 50%;
   transform: translateX(-50%);
@@ -11,6 +11,7 @@ export const StyledContainer = styled.div`
   background-color: transparent;
   box-shadow: 0 8px 8px 0 rgba(25, 42, 70, 0.1);
   border-radius: 34px;
+  background-color: #fff;
 `;
 
 const ActiveBtnStyles = css`
@@ -21,7 +22,7 @@ export const StyledPageBtn = styled.button<StyledBtnProps>`
   display: flex;
   align-items: center;
   justify-content: center;
-  background: ${({theme}) => theme.paginationBackground};
+  background: ${({ theme }) => theme.paginationBackground};
   width: 37px;
   padding: 10px;
   color: #fff;
@@ -29,29 +30,29 @@ export const StyledPageBtn = styled.button<StyledBtnProps>`
   border: none;
   outline: none;
   font-size: 14px;
-  
+
   &:disabled {
     background: gray;
     cursor: not-allowed;
   }
-  
+
   &:active {
     background: #3bb2b8;
   }
-  
+
   &:first-child {
     border-radius: 34px 0 0 34px;
   }
-  
+
   &:last-child {
     border-radius: 0 34px 34px 0;
   }
-  
+
   transition: background-color, color 0.1s ease;
 
   &:hover {
     color: black;
   }
-  
+
   ${({ isActive }) => isActive && ActiveBtnStyles}
 `;
